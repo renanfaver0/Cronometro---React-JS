227cc55a11ab561c787c0db637065f4b5d92e5e7
renanfaver0 <89169958+renanfaver0@users.noreply.github.com>
2022-01-16 06:02:58 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,53 +3,53 @@ import React, { Component } from 'react';
 import './style.css'
 
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             numero: 0,
             botao: 'VAI'
         };
-    
+
         this.timer = null;
         this.vai = this.vai.bind(this);
         this.limpar = this.limpar.bind(this);
 
     }
 
-    vai(){
+    vai() {
         let state = this.state;
 
-        if(this.timer !== null) {
+        if (this.timer !== null) {
             clearInterval(this.timer);
             this.timer = null;
             state.botao = 'VAI';
-        }else{
-            this.timer = setInterval(()=> {
+        } else {
+            this.timer = setInterval(() => {
                 let state = this.state;
                 state.numero += 0.1;
                 this.setState(state);
-            },100);
+            }, 100);
             state.botao = 'PAUSAR';
         }
-    
+
         this.setState(state);
     }
 
 
     limpar() {
-        if(this.timer !== null) {
+        if (this.timer !== null) {
             clearInterval(this.timer);
             this.timer = null;
+        }
+
+        let state = this.state;
+        state.numero = 0;
+        state.botao = 'VAI';
+        this.setState(state);
     }
 
-    let state = this.state;
-    state.numero = 0;
-    state.botao = 'VAI';
-    this.setState(state);
-}
-
     render() {
-        return(
+        return (
             <div className="container">
                 <img src={require('./assets/cronometro.png')} alt='Imagem de um cronometro em branco' className="img" />
                 <a className="timer">{this.state.numero.toFixed(1)}</a>
